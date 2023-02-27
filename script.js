e63601df28f4c38ac481618dc31048f953c70f6e
Chris Zins <92458306+cfz6677@users.noreply.github.com>
2023-02-27 12:18:30 -0700

    Changes

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Get references to the menu links and content sections
 const aboutLink = document.querySelector('nav ul li:nth-child(1) a');
 const pricingLink = document.querySelector('nav ul li:nth-child(2) a');
-const contactLink = document.querySelector('nav ul li:nth-child(3) a');
-const galleryLink = document.querySelector('nav ul li:nth-child(4) a');
+const stitchPatternsLink = document.querySelector('nav ul li:nth-child(3) a');
+const contactLink = document.querySelector('nav ul li:nth-child(4) a');
+const galleryLink = document.querySelector('nav ul li:nth-child(5) a');
 const aboutSection = document.querySelector('#about');
 const pricingSection = document.querySelector('#pricing');
+const stitchPatternsSection = document.querySelector('#stitch-patterns');
 const contactSection = document.querySelector('#contact');
 const gallerySection = document.querySelector('#gallery');
 
 // hide all sections except the first one
 pricingSection.style.display = 'none';
+stitchPatternsSection.style.display = 'none';
 contactSection.style.display = 'none';
 gallerySection.style.display = 'none';
 
@@ -18,6 +21,7 @@ aboutLink.addEventListener('click', event => {
   event.preventDefault();
   aboutSection.style.display = 'block';
   pricingSection.style.display = 'none';
+  stitchPatternsSection.style.display = 'none';
   contactSection.style.display = 'none';
   gallerySection.style.display = 'none';
 });
@@ -26,6 +30,16 @@ pricingLink.addEventListener('click', event => {
   event.preventDefault();
   pricingSection.style.display = 'block';
   aboutSection.style.display = 'none';
+  stitchPatternsSection.style.display = 'none';
+  contactSection.style.display = 'none';
+  gallerySection.style.display = 'none';
+});
+
+stitchPatternsLink.addEventListener('click', event => {
+  event.preventDefault();
+  stitchPatternsSection.style.display = 'block';
+  aboutSection.style.display = 'none';
+  pricingSection.style.display = 'none';
   contactSection.style.display = 'none';
   gallerySection.style.display = 'none';
 });
@@ -35,6 +49,7 @@ contactLink.addEventListener('click', event => {
   contactSection.style.display = 'block';
   aboutSection.style.display = 'none';
   pricingSection.style.display = 'none';
+  stitchPatternsSection.style.display = 'none';
   gallerySection.style.display = 'none';
 });
 
@@ -43,6 +58,7 @@ galleryLink.addEventListener('click', event => {
   gallerySection.style.display = 'block';
   aboutSection.style.display = 'none';
   pricingSection.style.display = 'none';
+  stitchPatternsSection.style.display = 'none';
   contactSection.style.display = 'none';
 });
 
