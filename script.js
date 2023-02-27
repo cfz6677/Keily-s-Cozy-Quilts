aaccff732f74b1cac4a9d70d9ffe91b7046035ab
Chris Zins <92458306+cfz6677@users.noreply.github.com>
2023-02-27 15:06:14 -0700

    Changes and Updates

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const pricingLink = document.querySelector('nav ul li:nth-child(2) a');
 const stitchPatternsLink = document.querySelector('nav ul li:nth-child(3) a');
 const contactLink = document.querySelector('nav ul li:nth-child(4) a');
 const galleryLink = document.querySelector('nav ul li:nth-child(5) a');
+const navBottomAboutLink = document.querySelector('.nav-bottom ul li:nth-child(1) a');
+const navBottomPricingLink = document.querySelector('.nav-bottom ul li:nth-child(2) a');
+const navBottomStitchPatternsLink = document.querySelector('.nav-bottom ul li:nth-child(3) a');
+const navBottomContactLink = document.querySelector('.nav-bottom ul li:nth-child(4) a');
+const navBottomGalleryLink = document.querySelector('.nav-bottom ul li:nth-child(5) a');
 const aboutSection = document.querySelector('#about');
 const pricingSection = document.querySelector('#pricing');
 const stitchPatternsSection = document.querySelector('#stitch-patterns');
@@ -16,51 +21,67 @@ stitchPatternsSection.style.display = 'none';
 contactSection.style.display = 'none';
 gallerySection.style.display = 'none';
 
-// add event listeners to the navigation links
-aboutLink.addEventListener('click', event => {
-  event.preventDefault();
-  aboutSection.style.display = 'block';
+// function to hide all sections and show the specified section
+function showSection(section) {
+  aboutSection.style.display = 'none';
   pricingSection.style.display = 'none';
   stitchPatternsSection.style.display = 'none';
   contactSection.style.display = 'none';
   gallerySection.style.display = 'none';
+  section.style.display = 'block';
+}
+
+// add event listeners to the navigation links
+aboutLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(aboutSection);
 });
 
 pricingLink.addEventListener('click', event => {
   event.preventDefault();
-  pricingSection.style.display = 'block';
-  aboutSection.style.display = 'none';
-  stitchPatternsSection.style.display = 'none';
-  contactSection.style.display = 'none';
-  gallerySection.style.display = 'none';
+  showSection(pricingSection);
 });
 
 stitchPatternsLink.addEventListener('click', event => {
   event.preventDefault();
-  stitchPatternsSection.style.display = 'block';
-  aboutSection.style.display = 'none';
-  pricingSection.style.display = 'none';
-  contactSection.style.display = 'none';
-  gallerySection.style.display = 'none';
+  showSection(stitchPatternsSection);
 });
 
 contactLink.addEventListener('click', event => {
   event.preventDefault();
-  contactSection.style.display = 'block';
-  aboutSection.style.display = 'none';
-  pricingSection.style.display = 'none';
-  stitchPatternsSection.style.display = 'none';
-  gallerySection.style.display = 'none';
+  showSection(contactSection);
 });
 
 galleryLink.addEventListener('click', event => {
   event.preventDefault();
-  gallerySection.style.display = 'block';
-  aboutSection.style.display = 'none';
-  pricingSection.style.display = 'none';
-  stitchPatternsSection.style.display = 'none';
-  contactSection.style.display = 'none';
+  showSection(gallerySection);
 });
+
+navBottomAboutLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(aboutSection);
+});
+
+navBottomPricingLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(pricingSection);
+});
+
+navBottomStitchPatternsLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(stitchPatternsSection);
+});
+
+navBottomContactLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(contactSection);
+});
+
+navBottomGalleryLink.addEventListener('click', event => {
+  event.preventDefault();
+  showSection(gallerySection);
+});
+
 
 // Embed the Google Form in the Pricing section
 const pricingSectionContent = document.querySelector('#pricing .content');
